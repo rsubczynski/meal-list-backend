18db74fa039e59a0cff2d953097aa23521e1a71d
SubczynskiR <radoslaw.subczynski@ts.fujitsu.com>
2020-06-15 12:50:06 +0200

    clean project

--- src/main/java/com/meal/list/backend/BackendApplication.java
+++ src/main/java/com/meal/list/backend/BackendApplication.java
@@ -1,22 +1,10 @@
 package com.meal.list.backend;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 
 @SpringBootApplication
 public class BackendApplication {
-
-
-	@Bean
-	ApplicationRunner applicationRunner (GreetingRepository greetingRepository){
-		return args -> {
-			greetingRepository.save(new Greeting("Hello"));
-			greetingRepository.save(new Greeting("Dziala"));
-		};
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
